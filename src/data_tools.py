90551b554f59dfd16152950093e9ad03c87d7d6a
ivallesp <ivanvallesperez@gmail.com>
2019-07-25 09:52:07 +0200

    Add timeless master table and reduce memory function

--- src/data_tools.py
+++ src/data_tools.py
@@ -229,16 +229,16 @@ class MasterDataGetter(DataGetter):
         self.fl_holidays = FactoryLoader()
         self.fl_oil = FactoryLoader()
         self.fl_transactions = FactoryLoader()
-        self.fl_stores = FactoryLoader()
+        # self.fl_stores = FactoryLoader()
         self.fl_main = FactoryLoader()
-        self.fl_items = FactoryLoader()
+        # self.fl_items = FactoryLoader()
 
         # Data loading
         df_holidays = self.fl_holidays.load("holidays_events")
         df_oil = self.fl_oil.load("oil")
         df_transactions = self.fl_transactions.load("transactions")
-        df_stores = self.fl_stores.load("stores")
-        df_items = self.fl_items.load("items")
+        # df_stores = self.fl_stores.load("stores")
+        # df_items = self.fl_items.load("items")
         df_main = self.fl_main.load("train")
 
         if sample:
@@ -253,9 +253,9 @@ class MasterDataGetter(DataGetter):
         del df_main, df_cartesian
 
         # Merge stores
-        df = df.merge(df_stores, on=self.fl_stores.getter.keys, how="left")
-        del df_stores; gc.collect()
-        print("Stores merged successfully!", df.shape)
+        # df = df.merge(df_stores, on=self.fl_stores.getter.keys, how="left")
+        # del df_stores; gc.collect()
+        # print("Stores merged successfully!", df.shape)
 
         # Merge holidays
         df = df.merge(df_holidays, on=self.fl_holidays.getter.keys, how="left")
@@ -263,9 +263,9 @@ class MasterDataGetter(DataGetter):
         print("Holidays merged successfully!", df.shape)
 
         # Merge items
-        df = df.merge(df_items, on=self.fl_items.getter.keys, how="left")
-        del df_items; gc.collect()
-        print("Items merged successfully!", df.shape)
+        # df = df.merge(df_items, on=self.fl_items.getter.keys, how="left")
+        # del df_items; gc.collect()
+        # print("Items merged successfully!", df.shape)
 
         # Merge transactions
         df = df.merge(df_transactions, on=self.fl_transactions.getter.keys, how="left")
@@ -281,12 +281,50 @@ class MasterDataGetter(DataGetter):
     def process(self, df):
         df["onpromotion"] = df.onpromotion.astype(float)
         df = df.fillna(0)
-        df["year"] = (df["date"].str[0:4].astype(int) - 2015)/2  # Center and scale
-        df["month"] = (df["date"].str[5:7].astype(int)-6.5)/5.5  # Center and scale
-        df["day"] = (df["date"].str[8:].astype(int)-16)/15  # Center and scale
-        df["dayofweek"] = (pd.to_datetime(df["date"], format="%Y-%m-%d").dt.dayofweek-3)/3  # Center and scale
+        df["year"] = (df["date"].astype("str").str[0:4].astype(int) - 2015)/2  # Center and scale
+        df["month"] = (df["date"].astype("str").str[5:7].astype(int)-6.5)/5.5  # Center and scale
+        df["day"] = (df["date"].astype("str").str[8:].astype(int)-16)/15  # Center and scale
+        df["dayofweek"] = (pd.to_datetime(df["date"], format="%Y%m%d").dt.dayofweek-3)/3  # Center and scale
         for var in ["store_nbr", "item_nbr"]:
             df[var] = pd.Categorical(df[var]).codes
+        return df
+
+
+class MasterTimelessGetter(DataGetter):
+    data_source_name = "master_timeless"
+
+    def load_raw(self, sample=False):
+        self.fl_stores = FactoryLoader()
+        self.fl_main = FactoryLoader()
+        self.fl_items = FactoryLoader()
+
+        # Data loading
+        df_stores = self.fl_stores.load("stores")
+        df_items = self.fl_items.load("items")
+        df_main = self.fl_main.load("train")
+
+        if sample:
+            print("Sampling!")
+            df_main = df_main[df_main.store_nbr < 6]
+
+        print("Data loaded successfully!")
+
+        # Performing and merging Cartesian
+        df_cartesian = cartesian_multiple(df_main, ["date", "store_nbr", "item_nbr"])
+        df = df_cartesian[["store_nbr", "item_nbr"]].drop_duplicates()
+        del df_main, df_cartesian; gc.collect()
+
+        # Merge stores
+        df = df.merge(df_stores, on=self.fl_stores.getter.keys, how="left")
+        del df_stores; gc.collect()
+        print("Stores merged successfully!", df.shape)
+
+        # Merge items
+        df = df.merge(df_items, on=self.fl_items.getter.keys, how="left")
+        del df_items; gc.collect()
+        print("Items merged successfully!", df.shape)
+
+    def process(self, df):
         return df
 
 
@@ -348,3 +386,66 @@ def get_batcher_generator(data_cube, model, batch_size, colnames, shuffle_every_
         yield tf_batch
 
 
+def reduce_mem_usage(props):
+    start_mem_usg = props.memory_usage().sum() / 1024 ** 2
+    print("Memory usage of properties dataframe is :", start_mem_usg, " MB")
+    NAlist = []  # Keeps track of columns that have missing values filled in.
+    for col in props.columns:
+        if props[col].dtype != object:  # Exclude strings
+
+            # Print current column type
+            print("******************************")
+            print("Column: ", col)
+            print("dtype before: ", props[col].dtype)
+
+            # make variables for Int, max and min
+            IsInt = False
+            mx = props[col].max()
+            mn = props[col].min()
+
+            # Integer does not support NA, therefore, NA needs to be filled
+            if not np.isfinite(props[col]).all():
+                NAlist.append(col)
+                props[col].fillna(mn - 1, inplace=True)
+
+                # test if column can be converted to an integer
+            asint = props[col].fillna(0).astype(np.int64)
+            result = (props[col] - asint)
+            result = result.sum()
+            if result > -0.01 and result < 0.01:
+                IsInt = True
+
+            # Make Integer/unsigned Integer datatypes
+            if IsInt:
+                if mn >= 0:
+                    if mx < 255:
+                        props[col] = props[col].astype(np.uint8)
+                    elif mx < 65535:
+                        props[col] = props[col].astype(np.uint16)
+                    elif mx < 4294967295:
+                        props[col] = props[col].astype(np.uint32)
+                    else:
+                        props[col] = props[col].astype(np.uint64)
+                else:
+                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                        props[col] = props[col].astype(np.int8)
+                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                        props[col] = props[col].astype(np.int16)
+                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                        props[col] = props[col].astype(np.int32)
+                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                        props[col] = props[col].astype(np.int64)
+
+                        # Make float datatypes 32 bit
+            else:
+                props[col] = props[col].astype(np.float32)
+
+            # Print new column type
+            print("dtype after: ", props[col].dtype)
+            print("******************************")
+        # Print final result
+    print("___MEMORY USAGE AFTER COMPLETION:___")
+    mem_usg = props.memory_usage().sum() / 1024 ** 2
+    print("Memory usage is: ", mem_usg, " MB")
+    print("This is ", 100 * mem_usg / start_mem_usg, "% of the initial size")
+    return props, NAlist
