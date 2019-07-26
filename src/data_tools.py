2a6d3a38392bcb63a531ba431fdae30f520d143f
ivallesp <ivanvallesperez@gmail.com>
2019-07-26 09:46:15 +0200

    Fix memory reducer and make the master processors more verbose

--- src/data_tools.py
+++ src/data_tools.py
@@ -5,10 +5,88 @@ import inspect
 import gc
 import random
 
+from tqdm import tqdm
 import numpy as np
 from src.common_paths import get_data_path
 from src.general_utilities import batching
 from src.constants import numeric_feats, categorical_feats, target, batch_time_normalizable_feats, embedding_sizes
+
+
+def check_if_integer(column, tolerance=0.01):
+    """
+    Checks if a column can be converted to integer
+    :param column: numeric column (pd.Series)
+    :param tolerance: tolerance for the float-int casting (float)
+    :return: True if column can be converted to integer (bool)
+    """
+    casted = column.fillna(0).astype(np.int64)
+    result = (column - casted)
+    result = result.sum()
+    if result > -0.01 and result < 0.01:
+        return True
+    else:
+        return False
+
+
+def reduce_mem_usage(df, int_cast=True, obj_to_category=False, subset=None):
+    """
+    Iterate through all the columns of a dataframe and modify the data type to reduce memory usage.
+    :param df: dataframe to reduce (pd.DataFrame)
+    :param int_cast: indicate if columns should be tried to be casted to int (bool)
+    :param obj_to_category: convert non-datetime related objects to category dtype (bool)
+    :param subset: subset of columns to analyse (list)
+    :return: dataset with the column dtypes adjusted (pd.DataFrame)
+    """
+    start_mem = df.memory_usage().sum() / 1024 ** 2;
+    gc.collect()
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    cols = subset if subset is not None else df.columns.tolist()
+
+    for col in tqdm(cols):
+        col_type = df[col].dtype
+
+        if col_type != object and col_type.name != 'category' and 'datetime' not in col_type.name:
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            # test if column can be converted to an integer
+            treat_as_int = str(col_type)[:3] == 'int'
+            if int_cast and not treat_as_int:
+                treat_as_int = check_if_integer(df[col])
+
+            if treat_as_int:
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.uint8).min and c_max < np.iinfo(np.uint8).max:
+                    df[col] = df[col].astype(np.uint8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.uint16).min and c_max < np.iinfo(np.uint16).max:
+                    df[col] = df[col].astype(np.uint16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
+                    df[col] = df[col].astype(np.uint32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+                elif c_min > np.iinfo(np.uint64).min and c_max < np.iinfo(np.uint64).max:
+                    df[col] = df[col].astype(np.uint64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        elif 'datetime' not in col_type.name and obj_to_category:
+            df[col] = df[col].astype('category')
+    gc.collect()
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    print('Memory usage after optimization is: {:.3f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
 
 
 def cartesian_pair(df1, df2, **kwargs):
@@ -280,7 +358,8 @@ class MasterDataGetter(DataGetter):
 
     def process(self, df):
         df = df.fillna(0)
-        df, _ = reduce_mem_usage(df)
+        print("NA Data filled with zeros successfully! Reducing the size of the dataset...")
+        df = reduce_mem_usage(df)
         df["onpromotion"] = df.onpromotion.astype(float)
         df["year"] = (df["date"].astype("str").str[0:4].astype(int) - 2015)/2  # Center and scale
         df["month"] = (df["date"].astype("str").str[5:7].astype(int)-6.5)/5.5  # Center and scale
@@ -327,6 +406,7 @@ class MasterTimelessGetter(DataGetter):
 
     def process(self, df):
         df = df.fillna(0)
+        print("NA Data filled with zeros successfully! Reducing the size of the dataset...")
         df = reduce_mem_usage(df)
         return df
 
@@ -387,68 +467,3 @@ def get_batcher_generator(data_cube, model, batch_size, colnames, shuffle_every_
         tf_batch[model.ph.target] = target_batch
 
         yield tf_batch
-
-
-def reduce_mem_usage(props):
-    start_mem_usg = props.memory_usage().sum() / 1024 ** 2
-    print("Memory usage of properties dataframe is :", start_mem_usg, " MB")
-    NAlist = []  # Keeps track of columns that have missing values filled in.
-    for col in props.columns:
-        if props[col].dtype != object:  # Exclude strings
-
-            # Print current column type
-            print("******************************")
-            print("Column: ", col)
-            print("dtype before: ", props[col].dtype)
-
-            # make variables for Int, max and min
-            IsInt = False
-            mx = props[col].max()
-            mn = props[col].min()
-
-            # Integer does not support NA, therefore, NA needs to be filled
-            if not np.isfinite(props[col]).all():
-                NAlist.append(col)
-                props[col].fillna(mn - 1, inplace=True)
-
-                # test if column can be converted to an integer
-            asint = props[col].fillna(0).astype(np.int64)
-            result = (props[col] - asint)
-            result = result.sum()
-            if result > -0.01 and result < 0.01:
-                IsInt = True
-
-            # Make Integer/unsigned Integer datatypes
-            if IsInt:
-                if mn >= 0:
-                    if mx < 255:
-                        props[col] = props[col].astype(np.uint8)
-                    elif mx < 65535:
-                        props[col] = props[col].astype(np.uint16)
-                    elif mx < 4294967295:
-                        props[col] = props[col].astype(np.uint32)
-                    else:
-                        props[col] = props[col].astype(np.uint64)
-                else:
-                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
-                        props[col] = props[col].astype(np.int8)
-                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
-                        props[col] = props[col].astype(np.int16)
-                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
-                        props[col] = props[col].astype(np.int32)
-                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
-                        props[col] = props[col].astype(np.int64)
-
-                        # Make float datatypes 32 bit
-            else:
-                props[col] = props[col].astype(np.float32)
-
-            # Print new column type
-            print("dtype after: ", props[col].dtype)
-            print("******************************")
-        # Print final result
-    print("___MEMORY USAGE AFTER COMPLETION:___")
-    mem_usg = props.memory_usage().sum() / 1024 ** 2
-    print("Memory usage is: ", mem_usg, " MB")
-    print("This is ", 100 * mem_usg / start_mem_usg, "% of the initial size")
-    return props, NAlist
