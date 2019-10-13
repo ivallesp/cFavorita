f5673a6622e9d00a5451735d1d7fdc43af14dbed
ivallesp <ivanvallesperez@gmail.com>
2019-10-13 21:08:12 +0200

    Add exceptions module

---
+++ src/exceptions.py
@@ -0,0 +1,14 @@
+class CredentialsError(Exception):
+    pass
+
+
+class QueryError(Exception):
+    pass
+
+
+class SchemaError(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    pass
