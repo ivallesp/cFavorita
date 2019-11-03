d70862e6be102bdeb1ebdb7c5de2df7cc2e74833
ivallesp <ivanvallesperez@gmail.com>
2019-11-03 19:45:33 +0100

    Fix chrontab

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,8 +141,8 @@ resource "aws_volume_attachment" "ebs_data_att" {
               # S3 sync
               aws s3 sync s3://phd-cfavorita/models /data/cFavorita/models
               aws s3 sync s3://phd-cfavorita/logs_tensorboard /data/.logs_tensorboard
-              (crontab -l ; echo "*/5 * * * * /usr/local/bin/aws s3 sync /data/cFavorita/models s3://phd-cfavorita/models") | sort - | uniq - | crontab -
-              (crontab -l ; echo "*/30 * * * * /usr/local/bin/aws s3 sync /data/.logs_tensorboard s3://phd-cfavorita/logs_tensorboard") | sort - | uniq - | crontab -
+              (crontab -l ; echo "* */3 * * * /usr/local/bin/aws s3 sync /data/cFavorita/models s3://phd-cfavorita/models") | sort - | uniq - | crontab -
+              (crontab -l ; echo "* */3 * * * /usr/local/bin/aws s3 sync /data/.logs_tensorboard s3://phd-cfavorita/logs_tensorboard") | sort - | uniq - | crontab -
 
               echo '{ "paths": {"data":  "data", "tensorboard": "/data/.logs_tensorboard"}}' > ./settings.json
 
