14f6c5e384f44bdf9c28b3bfef22958c88b5055a
ivallesp <ivanvallesperez@gmail.com>
2020-04-25 12:58:35 +0200

    Delete old architecture

--- src/architecture.py
+++ src/architecture.py
@@ -238,92 +238,6 @@ def torch_wrmse(actual, forecast, weight):
     return wrmse
 
 
-class Encoder(nn.Module):
-    def __init__(self, n_num_time_feats, categorical_cardinalities, cuda):
-        super().__init__()
-        self.cuda_ = cuda
-        self.embs = nn.ModuleDict()
-        for cat in categorical_cardinalities:
-            self.embs[cat] = nn.Embedding(
-                num_embeddings=categorical_cardinalities[cat],
-                embedding_dim=embedding_sizes[cat],
-                scale_grad_by_freq=False,
-            )
-
-        embs_sz = np.sum([embedding_sizes[c] for c in categorical_cardinalities.keys()])
-        input_sz = int(embs_sz + n_num_time_feats)
-        self.rnn_encoder = nn.LSTM(input_size=input_sz, hidden_size=128)
-
-    def forward(self, x_num_time, x_cat_time, cat_time_names):
-        emb_feats = []
-        for i, cat_feat_name in enumerate(cat_time_names):
-            emb_feats += [self.embs[cat_feat_name](x_cat_time[:, :, i].long())]
-        time_features = torch.cat([x_num_time] + emb_feats, -1).squeeze()
-        _, state = self.rnn_encoder(time_features)
-        return state
-
-
-class Decoder(nn.Module):
-    def __init__(self, n_forecast_timesteps, categorical_cardinalities, cuda):
-        super().__init__()
-        self.cuda_ = cuda
-        self.n_forecast_timesteps = n_forecast_timesteps
-        self.n_recurrent_cells = 128
-        self.rnn_decoder = nn.LSTM(input_size=1, hidden_size=self.n_recurrent_cells)
-        self.embs = nn.ModuleDict()
-
-        for cat in categorical_cardinalities:
-            self.embs[cat] = nn.Embedding(
-                num_embeddings=categorical_cardinalities[cat],
-                embedding_dim=embedding_sizes[cat],
-                scale_grad_by_freq=False,
-            )
-
-        embs_sz = np.sum([embedding_sizes[c] for c in categorical_cardinalities.keys()])
-        thought_sz = self.n_recurrent_cells * 2
-        context_thought_sz = thought_sz + embs_sz
-        cd_h1 = nn.Linear(in_features=context_thought_sz, out_features=512)
-        cd_h2 = nn.Linear(in_features=512, out_features=384)
-        cd_h3 = nn.Linear(in_features=384, out_features=thought_sz)
-        self.conditioning = nn.Sequential(
-            cd_h1, nn.ReLU(True), cd_h2, nn.ReLU(True), cd_h3
-        )
-        td_h1 = nn.Linear(in_features=self.n_recurrent_cells, out_features=128)
-        td_h2 = nn.Linear(in_features=128, out_features=1)
-        self.time_distributed = nn.Sequential(td_h1, nn.ReLU(True), td_h2)
-
-    def forward(self, x_cat_static, cat_static_names, state):
-        # Mock the input of the decoder
-        batch_size = x_cat_static.shape[0]
-        assert x_cat_static.shape[-1] == len(cat_static_names)
-
-        emb_feats = []
-        for i, cat_feat_name in enumerate(cat_static_names):
-            emb_feats += [self.embs[cat_feat_name](x_cat_static[:, i].long())]
-
-        thought = torch.cat(state, -1).squeeze()
-        context_thought = torch.cat(emb_feats + [thought], -1).squeeze()
-        context_thought = self.conditioning(context_thought)
-        context_thought = (
-            context_thought[:, : self.n_recurrent_cells].unsqueeze(0).contiguous(),
-            context_thought[:, self.n_recurrent_cells :].unsqueeze(0).contiguous(),
-        )
-
-        input_decoder = torch.zeros(
-            self.n_forecast_timesteps, context_thought[0].shape[1], 1
-        )
-        if self.cuda_:
-            input_decoder = input_decoder.cuda()
-        input_decoder[0, :, :] = 1  # GO!
-        # Concatenate the fwd looking features
-        # input_decoder = torch.cat([input_decoder, x_fwd], dim=2)
-        output, _ = self.rnn_decoder(input_decoder, context_thought)
-        h = output.reshape(self.n_forecast_timesteps * batch_size, output.shape[-1])
-        h = self.time_distributed(h)
-        output = h.reshape(self.n_forecast_timesteps, batch_size, 1).squeeze()
-        return output
-
-
 class EncoderTransformer(nn.Module):
     def __init__(self, d_model, pos_emb_size, n_input_feats, dropout, N=6, cuda=False):
 
@@ -455,15 +369,6 @@ class DecoderBlock(nn.Module):
         )
 
     def forward(self, y, h):
-        """[summary]
-
-        Args:
-            y ([type]): decoder previous outputs
-            h ([type]): encoder hidden state
-
-        Returns:
-            [type]: [description]
-        """
         # Decoder multi-head Attention layer
         h = self.attention_dec(query=y, key=y, value=y)  # self-attention.
         # Encoder-decoder multi-head Attention layer
